8dc2fe23ef0b851b73774da4c5bf0f2275129cb2
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-23 20:20:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,4 +32,10 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
 end
