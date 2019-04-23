1ec0d6419775915679ae5fafffd5359be95234f0
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-23 20:18:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,15 @@
 class SongsController < ApplicationController
   def new
+    @song = Song.new
   end
 
   def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to(@song)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,8 +22,10 @@ class SongsController < ApplicationController
   end
 
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 end
